b854907e21ec7b351ef47523f139bb9e30d014e4
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-04-27 17:37:48 +0300

    Update.

---
+++ pkg/starlark/starlark.go
@@ -0,0 +1,69 @@
+// Package starlark is executor for starklark.
+package starlark
+
+import (
+	"github.com/pkg/errors"
+	"go.starlark.net/resolve"
+	"go.starlark.net/starlark"
+
+	"github.com/percona-platform/saas/pkg/check"
+)
+
+// Run executes the script with given name and input data.
+func Run(name, script string, input []map[string]interface{}) (*check.Result, error) {
+	thread := &starlark.Thread{
+		Name: name,
+	}
+
+	rows, err := prepareRows(input)
+	if err != nil {
+		return nil, err
+	}
+
+	globals, err := starlark.ExecFile(thread, script, []byte(script), nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to execute starlark script")
+	}
+
+	f := globals["check"]
+	if f == nil {
+		return nil, errors.New("check function is not defined")
+	}
+
+	v, err := starlark.Call(thread, f, starlark.Tuple{rows}, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to execute check function")
+	}
+
+	switch v := v.(type) {
+	case *starlark.Dict:
+		// TODO https://jira.percona.com/browse/SAAS-84
+		return &check.Result{
+			Status:  "status",
+			Message: "message",
+		}, nil
+	default:
+		return nil, errors.Errorf("unhandled result type %T", v)
+	}
+}
+
+func prepareRows(input []map[string]interface{}) (starlark.Tuple, error) {
+	rows := make(starlark.Tuple, len(input))
+	for i, v := range input {
+		sv, err := goToStarlark(v)
+		if err != nil {
+			return nil, err
+		}
+		rows[i] = sv
+	}
+	rows.Freeze()
+
+	return rows, nil
+}
+
+// modify unavoidable global state once on package initialization to avoid race conditions
+//nolint:gochecknoinits
+func init() {
+	resolve.AllowFloat = true
+	resolve.AllowSet = true
+}
