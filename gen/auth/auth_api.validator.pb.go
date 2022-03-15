92ad1de42182d3e17e4480cc09f41452c80f71fb
Artem Gavrilov <artem.gavrilov@percona.com>
2022-03-15 22:19:34 +0200

    Update

--- gen/auth/auth_api.validator.pb.go
+++ gen/auth/auth_api.validator.pb.go
@@ -6,14 +6,12 @@ package authv1
 import (
 	fmt "fmt"
 	math "math"
-
 	proto "github.com/golang/protobuf/proto"
+	_ "github.com/mwitkow/go-proto-validators"
+	_ "google.golang.org/protobuf/types/known/timestamppb"
+	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2/options"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
-	_ "google.golang.org/genproto/googleapis/api/annotations"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
-
-	_ "github.com/percona-platform/saas/gen/validator"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
