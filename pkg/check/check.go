ed4a7ab633a1a1408078d460a7cb868e883e7cce
Artem Gavrilov <artem.gavrilov@percona.com>
2022-04-20 18:10:08 +0200

    Sync with platform

--- pkg/check/check.go
+++ pkg/check/check.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/blake2b"
@@ -127,6 +128,8 @@ const (
 	MongoDBGetCmdLineOpts    = Type("MONGODB_GETCMDLINEOPTS")
 	MongoDBReplSetGetStatus  = Type("MONGODB_REPLSETGETSTATUS")
 	MongoDBGetDiagnosticData = Type("MONGODB_GETDIAGNOSTICDATA")
+	MetricsInstant           = Type("METRICS_INSTANT")
+	MetricsRange             = Type("METRICS_RANGE")
 )
 
 // Type represents check type.
@@ -152,11 +155,41 @@ func (t Type) Validate() error { // nolint:cyclop
 	case MongoDBReplSetGetStatus:
 		fallthrough
 	case MongoDBGetDiagnosticData:
+		fallthrough
+	case MetricsInstant:
+		fallthrough
+	case MetricsRange:
 		return nil
 	case "":
 		return errors.New("check type is empty")
 	default:
 		return errors.Errorf("unknown check type: %s", t)
+	}
+}
+
+// Supported DB families.
+const (
+	MySQL      = Family("MYSQL")
+	PostgreSQL = Family("POSTGRESQL")
+	MongoDB    = Family("MONGODB")
+)
+
+// Family represents monitored service family.
+type Family string
+
+// Validate validates check family.
+func (f Family) Validate() error {
+	switch f {
+	case MySQL:
+		fallthrough
+	case PostgreSQL:
+		fallthrough
+	case MongoDB:
+		return nil
+	case "":
+		return errors.New("check family is empty")
+	default:
+		return errors.Errorf("unknown check family: %s", f)
 	}
 }
 
@@ -186,16 +219,36 @@ func (i Interval) Validate() error {
 	}
 }
 
-// Check represents security check structure.
+// Parameter represents query parameter.
+type Parameter string
+
+// Available query parameters.
+const (
+	Lookback = Parameter("lookback")
+	Range    = Parameter("range")
+	Step     = Parameter("step")
+)
+
+// Query represents DB query of specified type.
+type Query struct {
+	Query      string
+	Type       Type
+	Parameters map[Parameter]string
+}
+
+// Check represents security check structure. Fields marked with v1 should not be used for version 2, and vice versa.
 type Check struct {
 	Version     uint32        `yaml:"version"`
 	Name        string        `yaml:"name"`
 	Summary     string        `yaml:"summary"`
 	Description string        `yaml:"description"`
-	Type        Type          `yaml:"type"`
+	Type        Type          `yaml:"type,omitempty"`     // for v1
+	Category    string        `yaml:"category,omitempty"` // optional for v1, required for v2
+	Family      Family        `yaml:"family,omitempty"`   // for v2
 	Tiers       []common.Tier `yaml:"tiers,flow,omitempty"`
 	Interval    Interval      `yaml:"interval,omitempty"`
-	Query       string        `yaml:"query,omitempty"`
+	Query       string        `yaml:"query,omitempty"`   // for v1
+	Queries     []Query       `yaml:"queries,omitempty"` // for v2
 	Script      string        `yaml:"script"`
 }
 
@@ -205,9 +258,6 @@ var nameRE = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
 // Validate validates check for minimal correctness.
 func (c *Check) Validate() error { //nolint: cyclop
 	var err error
-	if c.Version != 1 {
-		return errors.Errorf("unexpected version %d", c.Version)
-	}
 
 	if !nameRE.MatchString(c.Name) {
 		return errors.New("invalid check name")
@@ -218,14 +268,6 @@ func (c *Check) Validate() error { //nolint: cyclop
 	}
 
 	if err = c.Interval.Validate(); err != nil {
-		return err
-	}
-
-	if err = c.Type.Validate(); err != nil {
-		return err
-	}
-
-	if err = c.validateQuery(); err != nil {
 		return err
 	}
 
@@ -245,6 +287,60 @@ func (c *Check) Validate() error { //nolint: cyclop
 		return errors.New("description is empty")
 	}
 
+	switch c.Version {
+	case 1:
+		return c.validateV1()
+	case 2:
+		return c.validateV2()
+	default:
+		return errors.Errorf("unexpected version %d", c.Version)
+	}
+}
+
+func (c *Check) validateV1() error {
+	var err error
+	if err = c.Type.Validate(); err != nil {
+		return err
+	}
+
+	if err = validateQuery(c.Type, c.Query); err != nil {
+		return err
+	}
+
+	if c.Family != "" {
+		return errors.New("field 'family' is part of check format version 2 and can't be used in version 1")
+	}
+
+	if len(c.Queries) != 0 {
+		return errors.New("field 'queries' is part of check format version 2 and can't be used in version 1")
+	}
+
+	return nil
+}
+
+func (c *Check) validateV2() error {
+	var err error
+	if err = c.Family.Validate(); err != nil {
+		return err
+	}
+
+	if err = c.validateQueries(); err != nil {
+		return err
+	}
+
+	if c.Type != "" {
+		return errors.New("field 'type' is part of check format version 1 and can't be used in version 2")
+	}
+
+	if c.Query != "" {
+		return errors.New("field 'query' is part of check format version 1 and can't be used in version 2")
+	}
+
+	// category is optional for v1 so that existing checks can continue working, but we make it required for v2.
+	if c.Category == "" {
+		return errors.New("category is empty")
+	}
+
 	return nil
 }
 
@@ -256,8 +352,8 @@ func (c *Check) validateScript() error {
 	return nil
 }
 
-func (c *Check) validateQuery() error { // nolint:cyclop
-	switch c.Type {
+func validateQuery(typ Type, query string) error { // nolint:cyclop
+	switch typ {
 	case PostgreSQLShow:
 		fallthrough
 	case MongoDBGetParameter:
@@ -269,16 +365,166 @@ func (c *Check) validateQuery() error { // nolint:cyclop
 	case MongoDBReplSetGetStatus:
 		fallthrough
 	case MongoDBGetDiagnosticData:
-		if c.Query != "" {
-			return errors.Errorf("%s check type should have empty query", c.Type)
+		if query != "" {
+			return errors.Errorf("query should be empty for '%s' type", typ)
 		}
 	case PostgreSQLSelect:
 		fallthrough
 	case MySQLShow:
 		fallthrough
 	case MySQLSelect:
-		if c.Query == "" {
-			return errors.New("check query is empty")
+		fallthrough
+	case MetricsInstant:
+		fallthrough
+	case MetricsRange:
+		if query == "" {
+			return errors.New("query is empty")
+		}
+	}
+
+	return nil
+}
+
+func validateQueryParameters(typ Type, params map[Parameter]string) error { //nolint:cyclop
+	switch typ {
+	case PostgreSQLShow:
+		fallthrough
+	case MongoDBGetParameter:
+		fallthrough
+	case MongoDBBuildInfo:
+		fallthrough
+	case MongoDBGetCmdLineOpts:
+		fallthrough
+	case MongoDBReplSetGetStatus:
+		fallthrough
+	case MongoDBGetDiagnosticData:
+		fallthrough
+	case PostgreSQLSelect:
+		fallthrough
+	case MySQLShow:
+		fallthrough
+	case MySQLSelect:
+		if len(params) != 0 {
+			return errors.Errorf("query for '%s' type should not have any parameters", typ)
+		}
+	case MetricsInstant:
+		return validateParametersForMetricsInstantQuery(params)
+	case MetricsRange:
+		return validateParametersForMetricsRangeQuery(params)
+	}
+
+	return nil
+}
+
+func validateParametersForMetricsInstantQuery(params map[Parameter]string) error {
+	for param, value := range params {
+		if param != Lookback {
+			return errors.Errorf("unsupported parameter '%s' for instant metris query", param)
+		}
+
+		if _, err := time.ParseDuration(value); err != nil {
+			return errors.Wrapf(err, "failed to parse loopback parameter value '%s', it should be a duration", value)
+		}
+	}
+
+	return nil
+}
+
+func validateParametersForMetricsRangeQuery(params map[Parameter]string) error {
+	if _, ok := params[Range]; !ok {
+		return errors.New("query parameter 'range' is required for metrics range queries")
+	}
+
+	if _, ok := params[Step]; !ok {
+		return errors.New("query parameter 'step' is required for metrics range queries")
+	}
+
+	for param, value := range params {
+		switch param {
+		case Lookback, Range, Step:
+			if _, err := time.ParseDuration(value); err != nil {
+				return errors.Wrapf(err, "failed to parse '%s' parameter value %s, it should be a duration", param, value)
+			}
+		default:
+			return errors.Errorf("unsupported parameter '%s' for range metris query", param)
+		}
+	}
+
+	return nil
+}
+
+func (c *Check) validateQueries() error {
+	if len(c.Queries) == 0 {
+		return errors.New("check should have at least one query")
+	}
+
+	var err error
+	for _, q := range c.Queries {
+		if err = q.Type.Validate(); err != nil {
+			return err
+		}
+
+		if err = validateQuery(q.Type, q.Query); err != nil {
+			return err
+		}
+
+		if err = validateQueryParameters(q.Type, q.Parameters); err != nil {
+			return err
+		}
+	}
+
+	switch c.Family {
+	case MySQL:
+		return checkQueryForCompatibilityWithMySQLFamily(c.Queries)
+	case PostgreSQL:
+		return checkQueryForCompatibilityWithPostgreSQLFamily(c.Queries)
+	case MongoDB:
+		return checkQueryForCompatibilityWithMonogDBFamily(c.Queries)
+	default:
+		return errors.Errorf("unknown check family: %s", c.Family)
+	}
+}
+
+func checkQueryForCompatibilityWithMySQLFamily(queries []Query) error {
+	for _, q := range queries {
+		switch q.Type { //nolint:exhaustive
+		case MySQLShow:
+		case MySQLSelect:
+		case MetricsInstant:
+		case MetricsRange:
+		default:
+			return errors.Errorf("unsupported query type '%s' for mySQL family", q.Type)
+		}
+	}
+
+	return nil
+}
+
+func checkQueryForCompatibilityWithPostgreSQLFamily(queries []Query) error {
+	for _, q := range queries {
+		switch q.Type { //nolint:exhaustive
+		case PostgreSQLShow:
+		case PostgreSQLSelect:
+		case MetricsInstant:
+		case MetricsRange:
+		default:
+			return errors.Errorf("unsupported query type '%s' for postgreSQL family", q.Type)
+		}
+	}
+
+	return nil
+}
+
+func checkQueryForCompatibilityWithMonogDBFamily(queries []Query) error {
+	for _, q := range queries {
+		switch q.Type { //nolint:exhaustive
+		case MongoDBGetParameter:
+		case MongoDBBuildInfo:
+		case MongoDBGetCmdLineOpts:
+		case MetricsInstant:
+		case MetricsRange:
+		default:
+			return errors.Errorf("unsupported query type '%s' for mongoDB family", q.Type)
 		}
 	}
 
